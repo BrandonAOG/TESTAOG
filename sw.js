105c96d9c5a284ed018ec41a1fd41e6e4267ffc2
BrandonAOG <brandon@alwaysongenerators.com>
2026-07-04 07:05:57 -0400

    Update CACHE_NAME to version 2.3.2

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 //    2. Update CHANGELOG below with what changed
 // ============================================================
 
-var CACHE_NAME = 'aog-forms-v2.3.1';
+var CACHE_NAME = 'aog-forms-v2.3.2';
 var DEV_MODE   = false;
 
 // Tracks whether this SW instance has already run a precache repair pass
